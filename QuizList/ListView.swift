696ef470129fc270a5b2b741c44b6408b4d41875
Alexander von Below <alexander.von_below@telekom.de>
2020-08-30 11:29:23 +0200

    The ListView first version

---
+++ QuizList/ListView.swift
@@ -0,0 +1,88 @@
+//
+//  ListView.swift
+//  QuizList
+//
+//  Created by Alexander v. Below on 27.08.20.
+//  Copyright © 2020 None. All rights reserved.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var list: QuizList
+    @State var quizList = [QuizListElement]()
+    @State var editMode: EditMode = .active
+
+    internal func shuffle () -> [QuizListElement]{
+        var mutableList = [QuizListElement]()
+        for element in self.list {
+            mutableList.append(element)
+        }
+        return mutableList.shuffled()
+    }
+
+    init(list: QuizList) {
+        self.list = list
+
+        _quizList = State(initialValue: shuffle())
+    }
+
+    struct MyList: View {
+        let quizList: [QuizListElement]
+        let action: ((IndexSet, Int) -> Void)?
+        @Binding var editMode: EditMode
+
+        var body: some View {
+            List {
+                ForEach (0..<quizList.count) { i in
+                    Text(quizList[i].text)
+                }
+                .onMove(perform: action)
+            }
+            .environment(\.editMode, $editMode)
+        }
+    }
+
+    var body: some View {
+        NavigationView {
+            MyList(quizList: quizList, action: move, editMode: $editMode)
+            
+//        .navigationBarItems(trailing:
+//                                        Button("foo", action: {
+//                                            debugPrint("Faz")
+//                                        })
+//        )
+        }
+    }
+
+    var allInPlace: Bool {
+        return quizList == list
+    }
+
+    func move(from indices: IndexSet, to newOffset: Int) {
+        quizList.move(fromOffsets: indices, toOffset: newOffset)
+        if allInPlace {
+            editMode = .inactive
+        }
+    }
+}
+
+class ListViewHostingController: UIHostingController<ListView>, ListController {
+    let questionView = QuestionView(list: QuizList())
+
+    var list: QuizList! {
+        didSet {
+//            self.rootView = ListView(list: list)
+        }
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder, rootView: ListView(list: QuizList()))
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView(list: QuizList())
+    }
+}
