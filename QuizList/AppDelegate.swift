11661e78c55ff9dbc38196908027d7a126cffa36
Alexander von Below <alex@vonbelow.com>
2022-10-29 17:36:52 +0200

    QuizList Bundle can be opened as a file

--- QuizList/AppDelegate.swift
+++ QuizList/AppDelegate.swift
@@ -18,17 +18,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
-    func application(
-        _ application: UIApplication,
-        open url: URL, sourceApplication: String?,
-        annotation: Any) -> Bool {
-        print ("Deprecated Method called")
-        return true
-    }
-    
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
         // https://developer.apple.com/documentation/uikit/uiapplicationdelegate/1623112-application
-        print("open: options:")
+        
+        let fileName = url.lastPathComponent
+        let targetURL = FileManager.default.documentsDirURL
+            .appendingPathComponent(fileName)
+        do {
+            try FileManager.default.copyItem(
+                at: url,
+                to: targetURL)
+        } catch {
+            let nsError = error as NSError
+            if nsError.domain == NSPOSIXErrorDomain, nsError.code == 17 {
+                print("File '\(fileName)' exists")
+            }
+            print ("Unable to copy: \(error)")
+            return false
+        }
+        // We do not use files in-place
+        do {
+            try FileManager.default.removeItem(at: url)
+        } catch {
+            print ("Unable to delete file '\(fileName)'")
+        }
         return true
     }
 }
