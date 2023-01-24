083ca9278a19b03052dfc859082ea463fa3e0de7
Alexander von Below <alex@vonbelow.com>
2023-01-24 17:06:12 +0100

    Just added a little documentation

--- QuizList/Intents.swift
+++ QuizList/Intents.swift
@@ -6,6 +6,11 @@
 //  Copyright © 2022 Alexander v. Below. All rights reserved.
 //
 
+
+/// Dokumentation:
+/// https://developer.apple.com/documentation/appintents/providing-your-app-s-capabilities-to-system-services
+/// https://developer.apple.com/documentation/sirikit/offering_actions_in_the_shortcuts_app
+
 import Foundation
 import AppIntents
 
@@ -30,3 +35,53 @@ struct ListAllItems: AppIntent {
   
     static var openAppWhenRun: Bool = false
 }
+
+//@available(iOS 16, *)
+//enum QuizIntentError: Swift.Error, CustomLocalizedStringResourceConvertible {
+//    case general
+//    case message(_ message: String)
+//
+//    var localizedStringResource: LocalizedStringResource {
+//        switch self {
+//        case let .message(message): return "Error: \(message)"
+//        case .general: return "My general error"
+//        }
+//    }
+//}
+//
+//@available(iOS 16, *)
+//struct QuizAnItem: AppIntent {
+////    @Parameter(title: "Number")
+////    var itemNumber: Int?
+//    @Parameter(title: "Answer")
+//    var answer: String?
+//    
+//    static var title: LocalizedStringResource = "Quiz an Item"
+//
+//    @MainActor
+//    func perform() async throws -> some IntentResult {
+//        // I should put this into a convenience function
+//        var list: QuizList = QuizList()
+//        if let sharedList = try? QuizList(firstAt: ContainerURL()) {
+//            list = sharedList
+//        }
+//        
+//        var items = list.items
+//        
+//        guard let quizItem = items.randomElement() else {
+//            throw QuizIntentError.message("No Item found")
+//        }
+//        
+//        answer = quizItem.text
+//        
+//        items = items.filter{
+//            $0.text != quizItem.text
+//        }
+//        
+//        try $answer.requestDisambiguation(among: ["a", "b", "c", "d"])
+//            
+//        return .result()
+//    }
+//  
+//    static var openAppWhenRun: Bool = false
+//}
